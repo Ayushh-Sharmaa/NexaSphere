82539be6399e06a5c877a3c7da4758e9c8c1972d
Aman Kushwaha <amankushwaha61709@gmail.com>
2026-08-09 17:50:03 +0530

    fix: protect event resource management routes
    Signed-off-by: Aman Kushwaha <amankushwaha61709@gmail.com>

--- server/routes/eventResource.js
+++ server/routes/eventResource.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { requireStudentAuth } = require('../middleware/studentAuthMiddleware');
 
 const eventResourceController = require('../controllers/eventResourceController');
+
+router.use(requireStudentAuth);
 
 // Resource CRUD
 router.post('/', eventResourceController.createResource);
