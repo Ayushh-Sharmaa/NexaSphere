6f53916c818ca9a7c3e5766c4b099218f6bf2a29
Aman Kushwaha <amankushwaha61709@gmail.com>
2026-08-09 16:50:00 +0530

    fix: protect dynamic pricing admin endpoints
    Signed-off-by: Aman Kushwaha <amankushwaha61709@gmail.com>

--- server/routes/dynamicPricing.js
+++ server/routes/dynamicPricing.js
@@ -7,14 +7,15 @@ import {
   setAdminOverride,
   getAnalytics,
 } from '../controllers/dynamicPricingController.js';
+import { requireStudentAuth } from '../middleware/studentAuthMiddleware.js';
 
 const router = express.Router();
 
-router.get('/analytics/all', getAnalytics);
 router.get('/:eventId', getPricing);
 router.get('/transparency/:eventId', getPriceTransparency);
-router.post('/config/:eventId', upsertPricing);
-router.post('/recalculate/:eventId', recalculatePrice);
-router.post('/override/:eventId', setAdminOverride);
+router.post('/config/:eventId', requireStudentAuth, upsertPricing);
+router.post('/recalculate/:eventId', requireStudentAuth, recalculatePrice);
+router.post('/override/:eventId', requireStudentAuth, setAdminOverride);
+router.get('/analytics/all', requireStudentAuth, getAnalytics);
 
 export default router;
